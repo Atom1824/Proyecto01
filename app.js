0228aa785cd8cd98aef30ec2519698df21aef1a3
Atom <gabrielruizamaya@gmail.com>
2026-03-24 09:00:53 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const API_KEY = "acb3f11d6c8a8e87e9b7c3834624dc16";
+const Base_URL = "https://api.themoviedb.org/3";
+
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
+
+const Movie_grid = document.querySelector(".movie-grid");
+
+async function getPopulularMovies() {
+    
+}   
